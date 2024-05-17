a0c6fd7c199bfc659d5fb5b782e8da01f3725d94
Satyam Namdev <namdev2003satyam@gmail.com>
2024-05-17 11:34:39 +0530

    Ummmmmmmmmmm

--- server.py
+++ server.py
@@ -1,9 +1,5 @@
 import sys
 from gunicorn.app.wsgiapp import run
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 if __name__ == '__main__':
     sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
