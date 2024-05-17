6db1c39eb904b9b0e28f3875a16a6530625333b5
Satyam Namdev <namdev2003satyam@gmail.com>
2024-05-17 20:40:51 +0530

    Finally

--- app.py
+++ app.py
@@ -40,20 +40,43 @@ def home():
             <footer style="position: absolute;
             bottom: 0;
             width: 99vw;
-            background-color: #333;
-            color: #fff;          
+        
+            color: green;       
+            font-size:5vh;
+            padding: 10px 0px 10px 0px;   
             text-align: center;"> 
-            Made with &hearts; by NAMDEV </footer>
+            Made with <span style="color:red;"> &hearts; </span> ~ NAMDEV </footer>
             ''')
     
 cross_origin
 @app.route('/api')
 def api():
     return ('''
-        <h1 style="font-size:50px;"> Kya be? apna kaam kr na !</h1>
+        <h1 style="font-size:50px;"> Kya bhai? Hain? </h1>
+        
+            <div class="tenor-gif-embed" data-postid="17088362" data-share-method="host" data-aspect-ratio="1.34454" data-width="50%">
+            
+            <a href="https://tenor.com/view/dwayne-johnson-the-rock-wwe-world-wrestling-entertainment-wrestler-gif-17088362">
+            Dwayne Johnson The Rock GIF</a>from 
+            <a href="https://tenor.com/search/dwayne+johnson-gifs">Dwayne Johnson GIFs</a>
+            
+            </div> 
+
+            <footer style="position: absolute;
+            bottom: 0;
+            width: 99vw;
+        
+            color: green;       
+            font-size:5vh;
+            padding: 10px 0px 10px 0px;   
+            text-align: center;"> 
+            Made with <span style="color:red;"> &hearts; </span> ~ NAMDEV </footer>
+            
+            <script type="text/javascript" async src="https://tenor.com/embed.js">
+            </script>
     ''')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
     
